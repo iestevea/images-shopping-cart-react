a2a00ce086ea0d69d5e458e7ee9ba246a4eb27c1
Iciar <iciar.ea235@gmail.com>
2021-04-12 20:35:39 +0200

    fix: images

--- src/pods/kitties/api/kitties.mock-data.ts
+++ src/pods/kitties/api/kitties.mock-data.ts
@@ -1,24 +1,28 @@
 import { KittyApi } from "./kitties.api-model";
+import image1 from 'assets/images/kitty_1.png';
+import image2 from 'assets/images/kitty_2.png';
+import image3 from 'assets/images/kitty_3.png';
+import image4 from 'assets/images/kitty_4.png';
 
 export const mockKittiesList: KittyApi[] = [
   {
     id: "kitty-1",
-    pictureUrl: "./src/assets/images/kitty_1.png",
-    title: "Cachorro 1",
+    pictureUrl: image1,
+    title: "Gatito 1",
   },
   {
     id: "kitty-2",
-    pictureUrl: "./src/assets/images/kitty_2.png",
-    title: "Cachorro 2",
+    pictureUrl: image2,
+    title: "Gatito 2",
   },
   {
     id: "kitty-3",
-    pictureUrl: "./src/assets/images/kitty_3.png",
-    title: "Cachorro 3",
+    pictureUrl: image3,
+    title: "Gatito 3",
   },
   {
     id: "kitty-4",
-    pictureUrl: "./src/assets/images/kitty_4.png",
-    title: "Cachorro 4",
+    pictureUrl: image4,
+    title: "Gatito 4",
   }
 ]
--- src/pods/puppies/api/puppies.mock-data.ts
+++ src/pods/puppies/api/puppies.mock-data.ts
@@ -1,19 +1,22 @@
 import { PuppyApi } from "./puppies.api-model";
+import image1 from 'assets/images/puppy_1.png';
+import image2 from 'assets/images/puppy_2.png';
+import image3 from 'assets/images/puppy_3.png';
 
 export const mockPuppiesList: PuppyApi[] = [
   {
     id: "puppy-1",
-    pictureUrl: "./src/assets/images/puppy_1.png",
-    title: "Cachorro 1",
+    pictureUrl: image1,
+    title: "Perrito 1",
   },
   {
     id: "puppy-2",
-    pictureUrl: "./src/assets/images/puppy_2.png",
-    title: "Cachorro 2",
+    pictureUrl: image2,
+    title: "Perrito 2",
   },
   {
     id: "puppy-3",
-    pictureUrl: "./src/assets/images/puppy_3.png",
-    title: "Cachorro 3",
+    pictureUrl: image3,
+    title: "Perrito 3",
   }
 ]
